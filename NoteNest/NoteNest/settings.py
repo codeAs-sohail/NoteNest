080bd48a584bbdd2c2d62758ecf0b4cca59102f3
codeAs-sohail <mohdsohail7x@gmail.com>
2026-06-10 16:40:47 +0530

    fix smpt conig

--- NoteNest/NoteNest/settings.py
+++ NoteNest/NoteNest/settings.py
@@ -155,8 +155,8 @@ SIMPLE_JWT={
 
 #Email Config
 DEFAULT_FROM_EMAIL=os.getenv("DEFAULT_FROM_EMAIL")
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
